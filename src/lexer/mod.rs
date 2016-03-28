1045f12af84f9c6f8a23b174a4265523596717e1
Yohai Berreby (yberreby) <yohaiberreby@gmail.com>
2016-03-28 19:22:56 +0200

    add Lexer stub

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,9 +1,32 @@
+use std::iter::Iterator;
+
 mod token;
 pub use self::token::*;
 
 #[cfg(test)]
 mod test;
 
+pub struct Lexer<'src> {
+    source: &'src str,
+}
+
+impl<'src> Lexer<'src> {
+    pub fn new(s: &str) -> Lexer {
+        Lexer { source: s }
+    }
+}
+
+impl<'src> Iterator for Lexer<'src> {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Token> {
+        unimplemented!()
+    }
+}
+
 pub fn tokenize(s: &str) -> Vec<Token> {
+    // Starting with an inline implementation, will break up into smaller pieces later.
+    let mut lexer = Lexer::new(s);
+    let tokens: Vec<Token> = lexer.collect();
     unimplemented!()
 }
