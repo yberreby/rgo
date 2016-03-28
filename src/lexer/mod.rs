37fdd6127190dffdd59b6c315f923e1f4bcd89f1
Yohai Berreby (yberreby) <yohaiberreby@gmail.com>
2016-03-28 19:34:05 +0200

    wip: expand lexer

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -8,11 +8,25 @@ mod test;
 
 pub struct Lexer<'src> {
     source: &'src str,
+    // XXX: is 'char' the right choice? Shouldn't we use u8? Not sure.
+    /// The last character to be read.
+    current_char: Option<char>,
 }
 
 impl<'src> Lexer<'src> {
     pub fn new(s: &str) -> Lexer {
-        Lexer { source: s }
+        let mut l = Lexer {
+            source: s,
+            current_char: Some('\n'), // This is dummy temporary value which is never read.
+        };
+
+        l.bump();
+        l
+    }
+
+    /// 'eat' one character.
+    fn bump(&mut self) {
+        unimplemented!()
     }
 }
 
@@ -20,6 +34,36 @@ impl<'src> Iterator for Lexer<'src> {
     type Item = Token;
 
     fn next(&mut self) -> Option<Token> {
+        let c = self.current_char;
+
+        match c.unwrap() {
+            '(' => {
+                self.bump();
+                return Some(Token::OpenDelim(DelimToken::Paren));
+            }
+            ')' => {
+                self.bump();
+                return Some(Token::CloseDelim(DelimToken::Paren));
+            }
+            '{' => {
+                self.bump();
+                return Some(Token::OpenDelim(DelimToken::Brace));
+            }
+            '}' => {
+                self.bump();
+                return Some(Token::CloseDelim(DelimToken::Brace));
+            }
+            '[' => {
+                self.bump();
+                return Some(Token::OpenDelim(DelimToken::Bracket));
+            }
+            ']' => {
+                self.bump();
+                return Some(Token::CloseDelim(DelimToken::Bracket));
+            }
+            _ => panic!(),
+        }
+
         unimplemented!()
     }
 }
