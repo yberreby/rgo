ad739d422498c2b7c3cfe1d9502325a138a1d227
Yohai Berreby (yberreby) <yohaiberreby@gmail.com>
2016-04-09 13:27:38 +0200

    parser: WIP - top-level fns

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -19,10 +19,18 @@ impl Parser {
 
     /// Parse the tokens into a SourceFile (AST).
     pub fn parse(mut self) -> SourceFile {
-        // Whitespace at the top of the file is irrelevant.
-        self.skip_ws();
-        self.expect(Token::Keyword(Keyword::Package));
+        let package_name = self.parse_package_clause();
+        let import_decls = self.parse_import_decls();
+        let top_level_decls = self.parse_top_level_decls();
 
+        SourceFile {
+            package: package_name,
+            import_decls: import_decls,
+            top_level_decls: top_level_decls,
+        }
+    }
+
+    fn bump(&mut self) -> Token {
         unimplemented!()
     }
 
@@ -32,6 +40,27 @@ impl Parser {
 
     /// Consume the next token, asserting it is equal to `expected`.
     fn expect(&mut self, expected: Token) {
+        unimplemented!()
+    }
+
+    fn parse_package_clause(&mut self) -> String {
+        // Whitespace at the top of the file is irrelevant.
+        self.skip_ws();
+        self.expect(Token::Keyword(Keyword::Package));
+
+        let t = self.bump();
+        match t {
+            Token::Ident(s) => s,
+            _ => panic!("expected identifier"),
+        }
+    }
+
+    fn parse_import_decls(&mut self) -> Vec<ImportDecl> {
+        unimplemented!()
+    }
+
+
+    fn parse_top_level_decls(&mut self) -> Vec<TopLevelDecl> {
         unimplemented!()
     }
 }
