83ed5845dd888748075e6ca54b133c548fe9527e
Yohai Berreby (yberreby) <yohaiberreby@gmail.com>
2016-04-15 13:29:13 +0200

    bench: add benchmarks for large files
    These are the two largest files in the official Go repository.

---
+++ benches/parse.rs
@@ -0,0 +1,40 @@
+#![feature(test)]
+
+
+/// Utility macro to bench test files one by one.
+macro_rules! bench_lex {
+    ($name:ident, $path:expr) => {
+        #[allow(non_snake_case)]
+        mod $name {
+            extern crate convenience;
+            extern crate test;
+            extern crate rgo;
+
+            use self::test::Bencher;
+            use std::path::Path;
+            use std::path::PathBuf;
+
+            fn test_path<P: AsRef<Path>>(path: P) -> PathBuf {
+                let mut new = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+                new.push("test-data");
+                new.push(path.as_ref());
+                new
+            }
+
+            #[bench]
+            fn bench_lex(b: &mut Bencher) {
+                let src = convenience::read_file(test_path($path)).unwrap();
+                b.bytes = src.len() as u64;
+
+                b.iter(|| {
+                    rgo::lexer::tokenize(&src);
+                });
+            }
+        }
+    }
+}
+
+bench_lex!(viper, "viper.go");
+bench_lex!(hello, "hello.go");
+bench_lex!(arithConst_ssa, "arithConst_ssa.go");
+bench_lex!(rewriteAMD64, "rewriteAMD64.go");
